7ceeca3032ca758397e09fc68fe9c83c2e61d11f
ritorads <ruben.diaz2017@alu.uct.cl>
2023-09-25 01:47:19 -0300

    funcionando

---
+++ MOVIL/app/src/main/java/com/example/compass1/RegisterActivity.kt
@@ -0,0 +1,76 @@
+package Compass1.Package
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.compass1.MainActivity
+import com.example.compass1.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+
+class RegisterActivity : AppCompatActivity() {
+    private lateinit var editTextName: EditText
+    private lateinit var editTextLastName: EditText
+    private lateinit var editTextEmail: EditText
+    private lateinit var editTextPassword: EditText
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        // Inicializar Firebase Authentication
+        auth = FirebaseAuth.getInstance()
+
+        // Inicializar vistas
+        editTextName = findViewById(R.id.editTextName)
+        editTextLastName = findViewById(R.id.editTextLastName)
+        editTextEmail = findViewById(R.id.editTextEmail)
+        editTextPassword = findViewById(R.id.editTextPassword)
+        val buttonRegister: Button = findViewById(R.id.buttonRegister)
+        val buttonBack: Button = findViewById(R.id.buttonBack)
+
+        // Configurar evento de clic para el botón de registro
+        buttonRegister.setOnClickListener {
+            val name = editTextName.text.toString()
+            val lastName = editTextLastName.text.toString()
+            val email = editTextEmail.text.toString()
+            val password = editTextPassword.text.toString()
+
+            // Verificar si los campos de entrada están vacíos
+            if (name.isEmpty() || lastName.isEmpty() || email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "Por favor, rellene el formulario", Toast.LENGTH_SHORT).show()
+            } else {
+                // Registrar al usuario en Firebase Authentication
+                auth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            // Registro exitoso, mostrar mensaje y redirigir al usuario a MainActivity_principal
+                            val user: FirebaseUser? = auth.currentUser
+                            Toast.makeText(this, "Registro completado", Toast.LENGTH_SHORT).show()
+
+                            // Redirigir al usuario a MainActivity_principal
+                            val intent = Intent(this, MainActivity::class.java)
+                            startActivity(intent)
+                            finish() // Cerrar la actividad actual
+                        } else {
+                            // Error en el registro, mostrar mensaje de error
+                            Toast.makeText(this, "Error en el registro: ${task.exception?.message}", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+            }
+        }
+
+        // Configurar evento de clic para el botón "Volver al Inicio"
+        buttonBack.setOnClickListener {
+            // Redirigir al usuario a MainActivity_principal
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish() // Cerrar la actividad actual
+        }
+    }
+}
+
